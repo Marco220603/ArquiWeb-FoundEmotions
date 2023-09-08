34f7ebc408ca5a35790123cb461ea3330415111c
Marco220603 <marcosponceboza@hotmail.com>
2023-09-08 17:02:12 -0500

    them.

--- src/main/java/com/example/demo/Controllers/CancionesController.java
+++ src/main/java/com/example/demo/Controllers/CancionesController.java
@@ -1,55 +1,15 @@
 package com.example.demo.Controllers;
 
-import com.example.demo.dtos.CancionesDTO;
-import com.example.demo.entities.Canciones;
 import com.example.demo.serviceinterfaces.ICancionService;
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-import java.util.stream.Collectors;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/canciones")
 public class CancionesController {
     @Autowired
-    private ICancionService aS;
+    private ICancionService cS;
+    
 
-    @PostMapping
-    public void registrar(@RequestBody CancionesDTO dto) {
-        ModelMapper m = new ModelMapper();
-        Canciones a = m.map(dto, Canciones.class);
-        aS.insertar(a);
-    }
-
-    @PutMapping
-    public void modificar(@RequestBody CancionesDTO dto) {
-        ModelMapper m = new ModelMapper();
-        Canciones a = m.map(dto, Canciones.class);
-        aS.insertar(a);
-    }
-
-    @DeleteMapping("/{id}")
-    public void eliminar(@PathVariable("id") Integer id) {
-        aS.eliminar(id);
-    }
-
-    @GetMapping("/{id}")
-    public CancionesDTO listarId(@PathVariable("id") Integer id) {
-        ModelMapper m=new ModelMapper();
-        CancionesDTO dto=m.map(aS.listarId(id),CancionesDTO.class);
-        return dto;
-    }
-
-    @GetMapping
-    @PreAuthorize("hasAuthority('ADMIN')")
-    public List<CancionesDTO> listar() {
-        return aS.listar().stream().map(x->{
-            ModelMapper m=new ModelMapper();
-            return m.map(x,CancionesDTO.class);
-
-        }).collect(Collectors.toList());
-    }
 }
